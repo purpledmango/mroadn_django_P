48630a8c33c4ca6aa24a2de7a970472053cc8323
purpledmango <nitishkchauhan98@gmail.com>
2023-10-13 09:24:28 +0530

    fixed view

--- blog/views.py
+++ blog/views.py
@@ -29,7 +29,12 @@ def home(request):
 	# the medium tile article of recent post 3rd article and on...
 	olderPosts = liveArticles[3:]
 	
-	suggested = olderPosts[3:len(olderPosts)-3]
+		# Assuming olderPosts is a list
+	if len(olderPosts) > 6:  # Check if there are at least 7 articles
+		suggested = olderPosts[3:len(olderPosts)-3]
+	else:
+		suggested = olderPosts  # Use all articles if there are 6 or fewer
+
 
 	# Handling the newsletter form
 	error = None
